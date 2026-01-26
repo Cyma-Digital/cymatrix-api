4340c1dba757ecea2f37072b5661e3096b143473
ryanolveir <ryandehallo229@gmail.com>
2026-01-26 15:40:38 -0300

    chore((tests)): change test framework from jest to vitest

--- jest.setup.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  testEnvironment: "node",
-  preset: "ts-jest",
-  verbose: true,
-}
--- tests/integration/api/status/get.test.ts
+++ tests/integration/api/status/get.test.ts
@@ -1,13 +1,15 @@
+import request from "supertest"
+import app from "../../../../src/app.js"
+
 describe("GET /api/status", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
-      const response = await fetch("http://localhost:3000/api/status")
-      const body = await response.json()
+      const response = await request(app).get("/api/status")
 
       expect(response.status).toBe(200)
-      expect(body.status).toBeDefined()
-      expect(body.database).toBeDefined()
-      expect(body.updated_at).toBeDefined()
+      expect(response.body.status).toBeDefined()
+      expect(response.body.database).toBeDefined()
+      expect(response.body.updated_at).toBeDefined()
     })
   })
 })
