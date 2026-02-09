b9302a3a7ac71039c2e05c28d704663234d3372e
ryanolveir <ryandehallo229@gmail.com>
2026-02-09 17:24:38 -0300

    test(integration): add test to GET `/api/users`

---
+++ tests/integration/api/user/get.test.ts
@@ -0,0 +1,70 @@
+import request from "supertest"
+import app from "@/app"
+import { orchestrator } from "tests/helpers/orchestrator"
+
+beforeEach(async () => {
+  await orchestrator.setup()
+})
+
+afterAll(async () => {
+  await orchestrator.tearDown()
+})
+
+describe("GET /api/users", () => {
+  describe("Anonymous user", () => {
+    describe("Success cases", () => {
+      test("should list all users", async () => {
+        await request(app).post("/api/users").send({
+          firstName: "Alessandro",
+          lastName: "Santos",
+          email: "alessandro_santos@gmail.com",
+          phone: "(15) 7614-8559",
+          document: "137.602.222-26",
+          documentType: "CPF",
+          password: "Test@123",
+          role: "ADMIN",
+          createdBy: 1,
+          updatedBy: 1,
+        })
+
+        await request(app).post("/api/users").send({
+          firstName: "Emanuelly",
+          lastName: "Martins",
+          email: "emanuelly.martins29@bol.com.br",
+          phone: "(97) 83440-8270",
+          document: "845.756.335-18",
+          documentType: "CPF",
+          password: "Test@123",
+          role: "STAFF",
+          createdBy: 1,
+          updatedBy: 1,
+        })
+
+        await request(app).post("/api/users").send({
+          firstName: "Fábio",
+          lastName: "Nogueira",
+          email: "fabio_nogueira63@hotmail.com",
+          document: "142.386.427-16",
+          documentType: "CPF",
+          password: "Test@123",
+          role: "CLIENT",
+          createdBy: 1,
+          updatedBy: 1,
+        })
+
+        const response = await request(app).get("/api/users")
+
+        expect(response.status).toBe(200)
+        expect(response.body.status).toBeDefined()
+        expect(response.body.status).toBe("success")
+        expect(response.body.data).toBeDefined()
+        expect(response.body).toMatchObject({
+          status: "success",
+          data: expect.any(Array),
+        })
+
+        expect(response.body.data).toHaveLength(4)
+      })
+    })
+  })
+})
