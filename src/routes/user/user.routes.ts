82db301ec1fd2782c83c64db663b037eb8be3077
ryanolveir <ryandehallo229@gmail.com>
2026-02-11 18:07:56 -0300

    feat: add PUT route for user update with validation

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express"
 import * as userController from "@/controllers/user/user.controller"
-import { validateParams } from "@/middlewares/validate.middleware"
-import { userIdSchema } from "@/schemas/user"
+import { validateBody, validateParams } from "@/middlewares/validate.middleware"
+import {
+  createUserSchama,
+  updateUserSchema,
+  userIdSchema,
+} from "@/schemas/user/user.schemas"
 
 const router = Router()
 
-router.post("/", userController.create)
+router.post("/", validateBody(createUserSchama), userController.create)
 router.get("/", userController.list)
 router.get("/:id", validateParams(userIdSchema), userController.getById)
+router.put("/:id", validateBody(updateUserSchema), userController.update)
 
 export default router
