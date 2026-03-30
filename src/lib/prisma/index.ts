6a6ca557b5e0ea6789d67641613b89f0f7124715
ryanolveir <ryandehallo229@gmail.com>
2026-03-30 17:37:51 -0300

    fix: enable ssl

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -3,9 +3,13 @@ import { PrismaPg } from "@prisma/adapter-pg"
 import { env } from "@/config/environment"
 
 const connectionString = env.DATABASE_URL
+const isLocal = connectionString.includes("localhost")
 
 const adapter = new PrismaPg(
-  { connectionString },
+  {
+    connectionString,
+    ssl: isLocal ? false : { rejectUnauthorized: false },
+  },
   {
     schema: env.POSTGRES_SCHEMA,
   },
