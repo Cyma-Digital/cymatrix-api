5748ada46bfb77c7ed39b2cae407f6c4cbd82167
Felipe Brigagão Santos <felibrisantos@gmail.com>
2026-03-30 11:11:34 -0300

    build(app): add cors dependency
    BREAKING CHANGE: cors was missing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import routes from "@/routes"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import { errorHandler } from "./middlewares"
 
 import morgan from "morgan"
 
 const app = express()
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use(morgan("dev"))
 app.use(cookieParser())
 app.use(express.json())
