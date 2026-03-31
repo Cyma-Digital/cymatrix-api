9feaffded2099a9e789c785fbf330de0a0d2226d
ryanolveir <ryandehallo229@gmail.com>
2026-03-31 09:30:56 -0300

    fix: disable ssl connection on prisma client

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -3,12 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg"
 import { env } from "@/config/environment"
 
 const connectionString = env.DATABASE_URL
-const isLocal = connectionString.includes("localhost")
 
 const adapter = new PrismaPg(
   {
     connectionString,
-    ssl: isLocal ? false : { rejectUnauthorized: false },
   },
   {
     schema: env.POSTGRES_SCHEMA,
