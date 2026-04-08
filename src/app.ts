41d06d1fa65fc82ae7d1e0bc52bd502b18ae8dd4
ryanolveir <ryandehallo229@gmail.com>
2026-04-08 10:43:47 -0300

    fix: cors origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import morgan from "morgan"
 
 const app = express()
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: true, credentials: true }))
 app.use(morgan("dev"))
 app.use(cookieParser())
 app.use(express.json())
