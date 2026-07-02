e7966177937ed37fb0ddfa0726c57b8feccb3c86
ryanolveir <ryandehallo229@gmail.com>
2026-07-02 15:29:52 -0300

    fix(effect): replace schedule.effect json column with ScheduleEffect entity

---
+++ prisma/migrations/20260702181344_remove_schedule_effect_json_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `effect` on the `schedule` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "schedule" DROP COLUMN "effect";
